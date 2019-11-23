587267c5031f4bd673d6ea1fb14db3ba073c9534
mlagarde <mathieu.lagarde@dbit.lu>
2019-11-23 16:23:54 +0100

    Validators: add pattern rule on creation date for questions

--- src/Validator/CreateQuestionRequestValidator.php
+++ src/Validator/CreateQuestionRequestValidator.php
@@ -16,4 +16,8 @@ class CreateQuestionRequestValidator extends RequestValidator
     public const OPTIONALS_FIELDS = [
         'createdAt'
     ];
+
+    public const FIELDS_PATTERN_RULES = [
+        'createdAt' => '/^[\d]{4}-[\d]{2}-[\d]{2}( [\d]{2}:[\d]{2}:[\d]{2})?$/'
+    ];
 }
